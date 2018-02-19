cf634ffc2a701bb6cb4e376daf87c3247a945172
HSS\r.jelier <rick@hollandshielding.com>
2018-02-19 16:17:36 +0100

    Note entity: added all fields

--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -16,5 +16,74 @@ class Note
      */
     private $id;
 
-    // add your own fields
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $message;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="notes")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Destination", inversedBy="notes")
+     */
+    private $destination;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestination()
+    {
+        return $this->destination;
+    }
+
+    /**
+     * @param mixed $destination
+     */
+    public function setDestination($destination)
+    {
+        $this->destination = $destination;
+    }
 }
