c4fa10ab8e166dab7cbe2e754c1c3ae6e75dea78
HSS\r.jelier <rick@hollandshielding.com>
2018-02-19 16:26:31 +0100

    Project entity: added all fields

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -16,5 +18,87 @@ class Project
      */
     private $id;
 
-    // add your own fields
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="createdProjects")
+     */
+    private $createdByUser;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="User", mappedBy="roles")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedByUser()
+    {
+        return $this->createdByUser;
+    }
+
+    /**
+     * @param mixed $createdByUser
+     */
+    public function setCreatedByUser($createdByUser)
+    {
+        $this->createdByUser = $createdByUser;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user)
+    {
+        if ($this->users->contains($user)) {
+            return;
+        }
+
+        $this->users[] = $user;
+        $user->addProject($this);
+    }
+
+    public function removeUser(User $user)
+    {
+        $this->users->removeElement($user);
+        $user->removeProject($this);
+    }
 }
