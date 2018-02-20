f4839ca89af5836c98f8e41d6ad6a9a865b22c6b
Rick Jelier <rick@hollandshielding.com>
2018-02-20 21:40:46 +0100

    Fixed all the admin interface bugs

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+class AdminController extends BaseAdminController
+{
+	protected function persistEntity($entity)
+	{
+		$this->em->persist($entity);
+		$this->em->flush();
+	}
+
+	protected function updateEntity($entity)
+	{
+		$this->em->flush();
+	}
+
+	protected function removeEntity($entity)
+	{
+		$this->em->remove($entity);
+		$this->em->flush();
+	}
+}
