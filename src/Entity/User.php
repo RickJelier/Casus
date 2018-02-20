319ccc2bbdce55bc8ff55d3d975b1398291673a9
HSS\r.jelier <rick@hollandshielding.com>
2018-02-20 10:31:41 +0100

    Forgot to generate getters/setters for User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -68,6 +68,62 @@ class User
     }
 
     /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
      * @return Collection|Destination[]
      */
     public function getDestinations()
